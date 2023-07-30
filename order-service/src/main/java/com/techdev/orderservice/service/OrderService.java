ffbafc848c3beddbce3046cf89b430b9d4644b84
VarelaTechDev <varela.tech.dev@gmail.com>
2023-07-30 19:42:36 -0400

    Before changing to inventory controller

--- order-service/src/main/java/com/techdev/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/techdev/orderservice/service/OrderService.java
@@ -8,6 +8,7 @@ import com.techdev.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 import java.util.UUID;
@@ -18,6 +19,8 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+
+    private final WebClient webClient;
 
     public void placeOrder(OrderRequest orderRequest){
         Order order = new Order();
@@ -30,7 +33,22 @@ public class OrderService {
 
         order.setOrderLineItemsList(orderLineItemsList);
 
-        orderRepository.save(order);
+        // Call Inventory Service, and place order if product is in stock
+        Boolean result =
+            webClient.get()
+                .uri("https://localhost:8082/api/inventory")
+                .retrieve()
+                .bodyToMono(Boolean.class)
+                .block();
+
+        if(result){
+            orderRepository.save(order);
+        }
+        else{
+            throw new IllegalArgumentException("Product is not in stock");
+        }
+
+
     }
 
     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
